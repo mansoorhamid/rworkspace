f7f3c691c9a382be5666db648f8fb506b224c8dd
User <mansoorhamid@gmail.com>
2019-11-13 20:02:13 +0000

    files to ignore

--- src/eda.R
+++ src/eda.R
@@ -1,8 +1,26 @@
 library('ProjectTemplate')
 load.project()
 
-for (dataset in project.info$data)
-{
-  message(paste('Showing top 5 rows of', dataset))
-  print(head(get(dataset)))
-}
+
+dim(Ch10Ex11)
+
+gexpr=read.csv("C:/Users/User/Desktop/Stats/Ch10Ex11.csv",header=TRUE)
+gexpr
+
+gexpr = t(gexpr)
+
+gexpr
+
+gexpr_dist = dist(gexpr)
+
+gexpr_matrix = as.matrix(gexpr)
+
+r_obs = cor(gexpr)
+d_cor = 1-r_obs
+
+d_cor = as.dist(d_cor)
+class(d_cor)
+hc_c = hclust(d_cor, method = "single")
+
+plot(hc_c, cex=0.5,main="",sub="",xlab="")
+
